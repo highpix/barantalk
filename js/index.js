c04df4af76ef310194f8a003da3b202e844dcb14
highpix <66276968+highpix@users.noreply.github.com>
2023-06-25 05:36:32 +0300

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,39 @@
+form = document.getElementById("loginForm");
+tokenInput = document.getElementById("tokenInput");
+
+function redirection() {
+    window.location.replace("chat/chat.html");
+}
+
+function checkToken() {
+    if (localStorage.getItem("token")) redirection();
+}
+checkToken();
+
+function notification(text) {
+    console.log(text);
+}
+
+async function getToken(token) {
+    let response = await fetch(`https://python.yagotka.repl.co/api/auth?token=${token}`, { 
+        method: "GET", 
+        mode: 'cors', 
+        headers: { 
+            'Content-Type': 'application/json', 
+            'ngrok-skip-browser-warning': 'true'
+        }
+    });
+
+    if (response.status == 200) {
+        localStorage.setItem("token", token);
+        notification("Authorized");
+        redirection();
+    } else {
+        notification(response.status == 400 ? "Missing parameters" : response.status == 401 ? "Unauthorized" : "Unknown error");
+    }
+}
+
+form.onsubmit = e => {
+    e.preventDefault();
+    getToken(tokenInput.value);
+};
